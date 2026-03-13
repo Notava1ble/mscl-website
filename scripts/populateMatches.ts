1780418119ce7fa07fb454f3026b0850af02a05c
Visar <visarmullaj07@gmail.com>
2026-03-13 19:41:23 +0100

    create script to update match history for match 1

--- scripts/populateMatches.ts
+++ scripts/populateMatches.ts
@@ -1,1 +1,38 @@
 import matches from "./data/wk1league5matches"
+
+const CONVEX_SITE_URL = process.env.CONVEX_SITE_URL
+const MY_API_KEY = process.env.WRITER_API_KEY || "test_key_placeholder"
+
+const ENDPOINT = `${CONVEX_SITE_URL}/api/write/match`
+
+type matchNumber = 1 | 2 | 3 | 4 | 5 | 6
+
+const prepareMatchData = (match: typeof matches, matchNumber: matchNumber) => {
+  return {
+    weekNumber: 1,
+    matchNumber: matchNumber,
+    leagueTier: 5,
+    results: match.matches[`${matchNumber}`]
+      .filter((p) => p.played === true)
+      .map((res) => ({
+        playerName: res.player,
+        pointsWon: res.pts,
+        timeMs: res.time * 1000, // convert seconds to ms
+      })),
+  }
+}
+
+for (let i = 1; i <= 6; i++) {
+  console.log(`Ingesting match ${i}...`)
+  const matchData = prepareMatchData(matches, i as matchNumber)
+
+  const res = await fetch(ENDPOINT, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "x-api-key": MY_API_KEY,
+    },
+    body: JSON.stringify(matchData),
+  })
+  console.log(await res.json())
+}
