86da2da3daeb68c3f6e06dae3702f8079b841330
Visar <visarmullaj07@gmail.com>
2026-03-19 18:42:27 +0100

    update scripts to work with the new API

--- scripts/populateMatches.ts
+++ scripts/populateMatches.ts
@@ -70,6 +70,7 @@ async function run() {
       const matchData = {
         weekNumber: WEEK_NUMBER,
         matchNumber: match.matchNumber,
+        rankedMatchId: match.matchId,
         leagueTier,
         results: match.results,
       }
